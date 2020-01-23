b38078766ad784644cc063877e9315fe2fe1dead
Richiely Paiva <richielybmp@hotmail.com>
2020-01-22 22:42:05 -0300

    Aula 3 - Criando a aplicação web para cadastrar spots.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -9,7 +11,9 @@ mongoose.connect("mongodb+srv://omnistack:omnistack@omnistack-rutpu.mongodb.net/
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Routes from './routes';
+import logo from './assets/logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="container">
+      <img src={logo} alt="aircnclogo" />
+
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
+  );
+}
+
+export default App;
