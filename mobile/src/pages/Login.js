47295e02919bf3042b795a697120f41e9e3cb163
Richiely Paiva <richielybmp@hotmail.com>
2020-01-23 22:34:41 -0300

    Aula  4 - Iniciando o app mobile.

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { KeyboardAvoidingView, View, StyleSheet, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+
+import logo from '../assets/logo.png';
+
+export default function Login() {
+    return (
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <Image source={logo} />
+
+            <View style={styles.form}>
+                <Text style={styles.label}>Seu e-mail *</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="e-mail"
+                    placeholderTextColor="#999"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+
+                <Text style={styles.label}>Tecnologias *</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="tecnologias de interesse"
+                    placeholderTextColor="#999"
+                    autoCapitalize='words'
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Encontrar spots</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30,
+    },
+
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: "#ddd",
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2
+    },
+
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+
+    buttonText: {
+        color: "#fff",
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
+});
